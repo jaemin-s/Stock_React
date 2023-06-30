555e2b3f5a86afe17b6d174732a8708601cf63b0
jihyeok <kidjh@naver.com>
2023-06-30 16:54:32 +0900

    feat: 네이버뉴스API 연동성공

--- src/component/news/NewsTest.js
+++ src/component/news/NewsTest.js
@@ -1,22 +1,35 @@
 import React from 'react'
+import { N_NEWS_ID, N_NEWS_KEY } from '../../config/apikey';
 
 const NewsTest = () => {
 
     const requestHeader = {
-        'X-Naver-Client-Id' : '4faR5kJNhKT_ZLX0EmNA',
-        'X-Naver-Client-Secret' : '62upEGk6oz'
+        'X-Naver-Client-Id' : N_NEWS_ID,
+        'X-Naver-Client-Secret' : N_NEWS_KEY 
     };
 
     const viewNews = async() =>{
        
-        const res = await fetch('https://openapi.naver.com/v1/search/news.json?query=삼성전자',{
+        const res = await fetch('/search/news.json?query=삼성전자',{
             headers : requestHeader
         });
         if(res.status === 200){
             const data = await res.json();
             console.log(data);
-            const temp = data.items;
-            console.log(temp);
+            let values = [];
+            
+            data.items.forEach( x => {
+                const { pubDate : date,
+                        link ,
+                        description,
+                        title
+                        } = x;
+                
+                values.push([
+                    date, link, description, title
+                ]);
+            });
+            console.log(values);
         }
     }
 
