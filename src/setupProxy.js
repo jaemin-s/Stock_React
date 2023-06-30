f7eac83dcb5423397c09a8ab1b3a5eb2c70934e8
jaemin <jaemin4033@gmail.com>
2023-06-30 15:13:11 +0900

    feat: api와 그래프 연동

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware('/uapi/domestic-stock/v1/quotations',{
-      target: 'https://openapi.koreainvestment.com:9443',
+    createProxyMiddleware('/quotations',{
+      target: 'https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1',
       changeOrigin: true
     })
   );
