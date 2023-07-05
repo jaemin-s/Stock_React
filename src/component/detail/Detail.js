a3ec877c258dfe8bde26f20f664330346523c828
jihyeok <kidjh@naver.com>
2023-07-05 18:14:53 +0900

    feat: 모달 수정 및 디테일스크립트 추가

--- src/component/detail/Detail.js
+++ src/component/detail/Detail.js
@@ -5,11 +5,159 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const Detail = () => {
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  //모달 관리
+  const [isModalOpen, setIsModalOpen] = useState(false); //매수
+  const [modalType, setModalType] = useState(false); //매도
+
+  //호가, 뉴스, 종목정보, 내주식 관리
+  const [stockPrice, setShowPrice] = useState(false);
+  const [news, setNews] = useState(false);
+  const [info, setInfo] = useState(false);
+  const [myStock, setMyStock] = useState(false);
+  
+  const showMyStock = () => {
+      setShowPrice(false);
+      setNews(false);
+      setInfo(false);
+      setMyStock(!myStock);
+  }
+
+  const showInfo = () => {
+      setShowPrice(false);
+      setNews(false);
+      setInfo(!info);
+      setMyStock(false);
+  }
+
+  const showNews = () => {
+      setShowPrice(false);
+      setNews(!news);
+      setInfo(false);
+      setMyStock(false);
+  }
+
+  const showPrice = () => {
+      setShowPrice(!stockPrice);
+      setNews(false);
+      setInfo(false);
+      setMyStock(false);
+  }
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
+  const sellModal = () => {
+    setModalType(!modalType);
+  };
+
+  const modalBuy = (
+    <>
+    <Modal isOpen={isModalOpen} toggle={toggleModal} style={{maxWidth: 2000,width: 600, marginTop: 200}}>
+        <ModalHeader toggle={toggleModal}>매수하기</ModalHeader>
+        <ModalBody>
+          {/* 여기에 모달 컨텐츠를 추가하세요 */}
+          <div id='modal-detail' className='flex'>
+            <div className='flex'>
+                <div className='box1'>
+                    <div>주문단가</div>
+                    <div>총 주문금액</div>
+                </div>
+                <div className='box2'>
+                    <div>12,120원</div>
+                    <div className='won'>원</div>
+                </div>
+            </div>
+            <div className='box3'>
+                <h5>주문수량</h5>
+                <input className='form-control bg-light border-0 small' placeholder='주' 
+                    type="number" min='1' style={{textAlign: 'right'}}/>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleModal}>매수</Button>
+          <Button color="secondary" onClick={toggleModal}>취소</Button>
+        </ModalFooter>
+        </Modal>
+    </>
+  );
+
+  const modalSell = (
+    <>
+    <Modal isOpen={modalType} toggle={sellModal} style={{maxWidth: 2000,width: 600, marginTop: 200}}>
+        <ModalHeader toggle={toggleModal}>매도하기</ModalHeader>
+        <ModalBody>
+          {/* 여기에 모달 컨텐츠를 추가하세요 */}
+          <div id='modal-detail' className='flex'>
+            <div className='flex'>
+                <div className='box1'>
+                    <div>주문단가</div>
+                    <div>총 주문금액</div>
+                </div>
+                <div className='box2'>
+                    <div>12,120원</div>
+                    <div className='won'>원</div>
+                </div>
+            </div>
+            <div className='box3'>
+                <h5>주문수량</h5>
+                <input className='form-control bg-light border-0 small' placeholder='주' 
+                    type="number" min='1' style={{textAlign: 'right'}}/>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={sellModal}>매도</Button>
+          <Button color="secondary" onClick={sellModal}>취소</Button>
+        </ModalFooter>
+        </Modal>
+    </>
+  );
+
+  const viewNews = (
+    <>
+    <div className="card-body">
+    <p>'코스피 지수 3000 돌파!'는 사라진 꿈이었나.. 잃어버린 우리의 코스피를 찾아서</p>
+    <hr/>
+    <p>대기업도 못 버틴 한한령… 현대차 中법인 매출 75% 급감했다</p>
+    <hr/>
+    <p>평택시, 쓰레기 무단투기 예방 순회 사진전 개최</p>
+    <hr/>
+    <p>반도체 한파 삼성전자, 성과급 전년 대비 반의 반토막</p>
+    <hr/>
+    </div>
+    </>
+  );
+
+  const viewPrice = (
+    <>
+    <div className="card-body">
+        <div>호가호가호가호가</div>
+    </div>
+    <div className='flex'>
+        <button className='btn btn-sm btn-user btn-primary' onClick={toggleModal}>매수</button>
+        <button className='btn btn-sm btn-user btn-danger' onClick={sellModal}>매도</button>
+    </div>
+        
+    </>
+  );
+
+  const viewInfo = (
+    <>
+    <div className="card-body">
+        <div>종목정보종목정보</div>
+    </div>
+    </>
+  );
+
+  const viewMyStock = (
+    <>
+    <div className="card-body">
+        <div>내 정보 내 정보</div>
+    </div>
+    </>
+  );
     
   return (
     <>
@@ -36,17 +184,16 @@ const Detail = () => {
                     <div id='third-box' className="popular-trade card shadow mb-4">
                         <div className="card-header">
                             <h6 className="m-0 font-weight-bold text-primary">
-                                <span className='border-bottom-primary'>호가</span>
-                                <span className='border-bottom-primary'>뉴스</span>
-                                <span className='border-bottom-primary'>종목정보</span>
-                                <span className='border-bottom-primary'>내주식</span>
+                                <span id='price' className='border-bottom-primary' onClick={showPrice}>호가</span>
+                                <span id='news' className='border-bottom-primary' onClick={showNews}>뉴스</span>
+                                <span id='info' className='border-bottom-primary' onClick={showInfo}>종목정보</span>
+                                <span id='myStock' className='border-bottom-primary' onClick={showMyStock}>내주식</span>
                             </h6>
                         </div>
-                        <div className="card-body">뉴스 내용</div>
-                        <div className='flex'>
-                            <button className='btn btn-sm btn-user btn-primary' onClick={toggleModal}>매수</button>
-                            <button className='btn btn-sm btn-user btn-google'>매도</button>
-                        </div>
+                            {stockPrice && viewPrice}
+                            {news && viewNews}
+                            {info && viewInfo}
+                            {myStock && viewMyStock}
                     </div>
                 </div>
                 <hr/>
@@ -68,33 +215,8 @@ const Detail = () => {
             </div>
         </div>
     </body>
-    
-    <Modal isOpen={isModalOpen} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>매수 모달창</ModalHeader>
-        <ModalBody>
-          {/* 여기에 모달 컨텐츠를 추가하세요 */}
-          <div id='modal-detail' className='flex'>
-            <div className='flex'>
-                <div className='box1'>
-                    <div>주문단가</div>
-                    <div>총 주문금액</div>
-                </div>
-                <div className='box2'>
-                    <div>12,120원</div>
-                    <div>원</div>
-                </div>
-            </div>
-            <div className='box3'>
-                <h5>주문수량</h5>
-                <input type="number"/ >주
-            </div>
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggleModal}>매수</Button>
-          <Button color="secondary" onClick={toggleModal}>취소</Button>
-        </ModalFooter>
-      </Modal>
+    {isModalOpen && modalBuy}
+    {modalType && modalSell}
     </>
   )
 }
--- src/component/stock/MoveStockInfo.js
+++ src/component/stock/MoveStockInfo.js
@@ -3,13 +3,22 @@ import Ticker from 'react-ticker'
 
 const MoveStockInfo = () => {
   return (
-    <Ticker mode='await' offset={5}>
-        {() => (
+    <>
+      <Ticker mode='await' offset={5}>
+          {() => (
             <>
-                <p>1번 주식 2번 주식 3번 주식</p>
-            </>
-        )}
-    </Ticker>
+                  <p>상승 1위 주식 상승 2위 주식 상승 3위 주식</p>
+              </>
+          )}
+      </Ticker>
+      <Ticker mode='await' offset={5} direction='toRight'>
+          {() => (
+            <>
+                  <p>하락 1위 주식 하락 2위 주식 하락 3위 주식</p>
+              </>
+          )}
+      </Ticker>
+    </>
   )
 }
 
