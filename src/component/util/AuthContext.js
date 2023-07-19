c83037a013093f1e4d2554d2835a769f65843d43
jihyeok <kidjh@naver.com>
2023-07-19 15:25:59 +0900

    feat: 디테일화면과 헤더 수정

--- src/component/util/AuthContext.js
+++ src/component/util/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
-isLoggedIn: false,
+  isLoggedIn: false,
   userEmail: "",
   userName: "",
   UserImage: "",
@@ -14,66 +14,60 @@ isLoggedIn: false,
   setUserInfo: () => {},
 });
 
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [userImage, setUserImage] = useState("");
+  const [userName, setUserName] = useState("");
+  const [nick, setNick] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [career, setCareer] = useState("");
 
-export const AuthContextProvider = props => {
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userEmail, setUserEmail] = useState(''); 
-    const [userImage, setUserImage] = useState('');
-    const [userName, setUserName] = useState("");
-    const [nick, setNick] = useState("");
-    const [age, setAge] = useState("");
-    const [gender, setGender] = useState("");
-    const [career, setCareer] = useState("");
-
-
-   
-    useEffect(() => {
-        if(localStorage.getItem('isLoggedIn') === '1') {
-            setIsLoggedIn(true);
-            setUserEmail(localStorage.getItem('LOGIN_USEREMAIL'));
-            setUserImage(localStorage.getItem('LOGIN_USERIMAGE'))
-        } 
-    }, []);
-
-
-    //로그아웃 핸들러
-    const logoutHandler = () => {
-        localStorage.clear();
-        setIsLoggedIn(false);
-    };
-
-    //로그인 핸들러
-    const loginHandler = (token, email, image) => {
-        localStorage.setItem('isLoggedIn', '1');
-        localStorage.setItem('LOGIN_ACCESS_TOKEN', token);
-        localStorage.setItem('LOGIN_USEREMAIL', email);
-        localStorage.setItem('LOGIN_USERIMAGE', image);
-        setIsLoggedIn(true);
-        setUserEmail(email);
-        setUserImage(image);
-    };
-
-    //카카오 로그인 핸들러
-    const kLoginHandler = (token, email, image) => {
-        localStorage.setItem('isLoggedIn', '1');
-        localStorage.setItem('LOGIN_ACCESS_TOKEN', token);
-        localStorage.setItem('LOGIN_USEREMAIL', email);
-        localStorage.setItem('LOGIN_USERIMAGE', image);
-        setIsLoggedIn(true);
-        setUserEmail(email);
-        setUserImage(image);
-    };
-
-    //토큰 및 로그인 유저 데이터를 브라우저에 저장하는 함수
-    const setLoginUserInfo = ({ token, email, image }) => {
-        localStorage.setItem('LOGIN_ACCESS_TOKEN', token);
-        localStorage.setItem('LOGIN_USEREMAIL', email);
-        localStorage.setItem('LOGIN_USERIMAGE', image);
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+      setUserEmail(localStorage.getItem("LOGIN_USEREMAIL"));
+      setUserImage(localStorage.getItem("LOGIN_USERIMAGE"));
     }
+  }, []);
 
+  //로그아웃 핸들러
+  const logoutHandler = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+  };
 
-    useEffect(() => {
+  //로그인 핸들러
+  const loginHandler = (token, email, image) => {
+    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem("LOGIN_ACCESS_TOKEN", token);
+    localStorage.setItem("LOGIN_USEREMAIL", email);
+    localStorage.setItem("LOGIN_USERIMAGE", image);
+    setIsLoggedIn(true);
+    setUserEmail(email);
+    setUserImage(image);
+  };
+
+  //카카오 로그인 핸들러
+  const kLoginHandler = (token, email, image) => {
+    localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem("LOGIN_ACCESS_TOKEN", token);
+    localStorage.setItem("LOGIN_USEREMAIL", email);
+    localStorage.setItem("LOGIN_USERIMAGE", image);
+    setIsLoggedIn(true);
+    setUserEmail(email);
+    setUserImage(image);
+  };
+
+  //토큰 및 로그인 유저 데이터를 브라우저에 저장하는 함수
+  const setLoginUserInfo = ({ token, email, image }) => {
+    localStorage.setItem("LOGIN_ACCESS_TOKEN", token);
+    localStorage.setItem("LOGIN_USEREMAIL", email);
+    localStorage.setItem("LOGIN_USERIMAGE", image);
+  };
+
+  useEffect(() => {
     if (localStorage.getItem("isLoggedIn") === "1") {
       setIsLoggedIn(true);
       setUserEmail(localStorage.getItem("LOGIN_USEREMAIL"));
@@ -82,24 +76,24 @@ export const AuthContextProvider = props => {
       setAge(localStorage.getItem("AGE"));
       setGender(localStorage.getItem("GENDER"));
       setCareer(localStorage.getItem("CAREER"));
-    } },[]);
+    }
+  }, []);
 
-
-    return (
-        <AuthContext.Provider value={{
-            isLoggedIn,
-            userEmail,
-            userImage,
-            onLogout: logoutHandler,
-            onLogin: loginHandler, kLoginHandler,
-            setUserInfo: setLoginUserInfo
-        }}>
-            {props.children}
-        </AuthContext.Provider>
-    );
-
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        userEmail,
+        userImage,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+        kLoginHandler,
+        setUserInfo: setLoginUserInfo,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
-
-
