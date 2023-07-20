50723d7bb1dc6b742cc469df7b931cfb972dfa55
jaemin <jaemin4033@gmail.com>
2023-07-20 16:52:25 +0900

    before merge

--- src/component/detail/Candle.js
+++ src/component/detail/Candle.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ECharts from "echarts-for-react";
 import { useParams } from "react-router-dom";
 const Candle = ({ dailyPrice }) => {
