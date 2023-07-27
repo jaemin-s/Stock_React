5bf5ad2201f34c222241bbab20b81bfed1dc04c7
jaemin <jaemin4033@gmail.com>
2023-07-27 17:37:15 +0900

    fix: 관리자 페이지 컴포넌트 분리

---
+++ src/component/adminPage/UserInfoTable.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Paging from "../board/Paging";
+
+const UserInfoTable = () => {
+  return (
+    <>
+      <div className="user-info-box">
+        <table className="table">
+          <thead>
+            <tr>
+              <th
+                style={{
+                  backgroundColor: "#3385ff",
+                  color: "white",
+                  fontWeight: "600",
+                }}
+                scope="col"
+              >
+                이름
+              </th>
+              <th
+                scope="col"
+                style={{
+                  backgroundColor: "#3385ff",
+                  color: "white",
+                  fontWeight: "600",
+                }}
+              >
+                닉네임
+              </th>
+              <th
+                scope="col"
+                style={{
+                  backgroundColor: "#3385ff",
+                  color: "white",
+                  fontWeight: "600",
+                }}
+              >
+                이메일
+              </th>
+              <th
+                scope="col"
+                style={{
+                  backgroundColor: "#3385ff",
+                  color: "white",
+                  fontWeight: "600",
+                }}
+              >
+                전화번호
+              </th>
+              <th
+                scope="col"
+                style={{
+                  backgroundColor: "#3385ff",
+                  color: "white",
+                  fontWeight: "600",
+                }}
+              >
+                관리
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>홍길동</td>
+              <td>춘식이</td>
+              <td>hong@gmail.com</td>
+              <td>01024093845</td>
+              <td>
+                <button
+                  //   onClick={toggleModal}
+                  style={{ border: "none", backgroundColor: "white" }}
+                >
+                  ↪
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <Paging />
+    </>
+  );
+};
+
+export default UserInfoTable;
