c8e517ce65596a8a4451c575b0f602baa27cf299
jaemin <jaemin4033@gmail.com>
2023-07-28 11:35:45 +0900

    feat: 관리자 페이지 유저통계, 거래내역

---
+++ src/component/adminPage/stats/UserGroupByCareer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import DoughnutMaker from "./DoughnutMaker";
+
+const UserGroupByCareer = () => {
+  return (
+    <section className="stats-by-MBTI admin-stats">
+      <h1 class="h3 text-gray-800 stats-header">경력</h1>
+      <div className="stats-body">
+        <div class="col-xl-4 col-lg-5">
+          <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">경력 별 유저 수</h6>
+            </div>
+            <div class="card-body">
+              <DoughnutMaker />
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-4 col-lg-5">
+          <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">
+                경력 별 평균 손익
+              </h6>
+            </div>
+            <div class="card-body">
+              <DoughnutMaker />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UserGroupByCareer;
