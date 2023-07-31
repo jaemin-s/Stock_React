628f10511b4bebdf571409cd47f9bd005a1260d4
jihyeok <kidjh@naver.com>
2023-07-31 15:01:32 +0900

    feat: mbti별 유저 수 기능 구현

--- src/component/adminPage/stats/UserGroupByCareer.js
+++ src/component/adminPage/stats/UserGroupByCareer.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DoughnutMaker from "./DoughnutMaker";
 
 const UserGroupByCareer = () => {
+  const [careerUser, setCareerUser] = useState();
+
+  async function getCareerUser() {
+    const res = await fetch("http://localhost:8181/api/user/careeruser");
+    if (res.status === 200) {
+      const data = await res.json();
+      console.log(data);
+      const labels = [];
+      const doughnutData = [];
+      data.forEach((item) => {
+        if (item.career == 1) {
+          labels.push("입문");
+        } else if (item.career == 2) {
+          labels.push("1~3년");
+        } else if (item.career == 3) {
+          labels.push("4~10년");
+        } else if (item.career == 4) {
+          labels.push("10년 이상");
+        } else {
+          labels.push("경력 체크 안함");
+        }
+        doughnutData.push(item.count);
+      });
+      setCareerUser({
+        labels: labels,
+        doughnutLabel: "유저 수",
+        doughnutData: doughnutData,
+      });
+    }
+  }
+
+  useEffect(() => {
+    getCareerUser();
+  }, []);
+
   return (
     <section className="stats-by-MBTI admin-stats">
       <h1 className="h3 text-gray-800 stats-header">경력</h1>
@@ -14,7 +49,11 @@ const UserGroupByCareer = () => {
               </h6>
             </div>
             <div className="card-body">
-              <DoughnutMaker />
+              <DoughnutMaker
+                labels={!!careerUser && careerUser.labels}
+                doughnutLabel={!!careerUser && careerUser.doughnutLabel}
+                doughnutData={!!careerUser && careerUser.doughnutData}
+              />
             </div>
           </div>
         </div>
