5066b87d4fdf78863a998bb9bcb71e45ebee17a2
KJH <0311wlsgus@gmail.com>
2023-08-02 10:08:58 +0900

    fix: RollControl modal 삭제

--- src/component/adminPage/RollControl.js
+++ src/component/adminPage/RollControl.js
@@ -25,35 +25,7 @@ const RollControl = ({ isOpen, toggleHandler, blackEmail }) => {
   };
 
   handleForceGradeDown();
-  return (
-    <Modal isOpen={isOpen} toggle={toggleHandler} style={{ width: 1000 }}>
-      <ModalBody>
-        <div className="roll-box" style={{ display: "flex" }}>
-          <div
-            className="roll-check"
-            style={{
-              width: 160,
-              margin: "0 auto",
-              height: 150,
-              textAlign: "center",
-              borderRight: "1px solid black",
-            }}
-          >
-            <p style={{ marginTop: 50 }}>홍길동님의 등급은 "브론즈" 입니다.</p>
-          </div>
-          <div>등급 지정하기</div>
-          <div className="roll-option">
-            <select>
-              <option>블랙</option>
-              <option>브론즈</option>
-              <option>실버</option>
-              <option>골드</option>
-            </select>
-          </div>
-        </div>
-      </ModalBody>
-    </Modal>
-  );
+  return <></>
 };
 
 export default RollControl;
